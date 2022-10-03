983d6f1c7c9a558f8f0e9ad864a560e19893c005
SONALI PATRO <sitalsonali@gmail.com>
2022-10-03 13:06:41 +0530

    Create Seven.java

---
+++ Seven.java
@@ -0,0 +1,21 @@
+package pattern;
+
+public class Seven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=5;i++) {
+			for(int j=5;j>=i;j--) {
+				System.out.print(" ");
+			}
+			for(int j=i-1;j>0;j--) {
+				System.out.print("*");
+			}
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
