5259318c6ce778ee04927fc8a31bf64b9e2c54b0
SONALI PATRO <sitalsonali@gmail.com>
2022-10-03 13:08:27 +0530

    Create Nine.java

---
+++ Nine.java
@@ -0,0 +1,34 @@
+package pattern;
+
+public class Nine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=5;i++) {
+			for(int j=5;j>=i;j--) {
+				System.out.print(" ");
+			}
+			for(int j=i-1;j>0;j--) {
+				System.out.print("*");
+			}
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(" ");
+			}
+			for(int j=i+1;j<=5;j++) {
+				System.out.print("*");
+			}
+			int k=5-i+1;
+			for(int j=k;j>=1;j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
